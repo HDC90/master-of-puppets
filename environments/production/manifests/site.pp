6e0d029b3b5e529233746f4261395f089dc7c04d
root <root@master.puppetmichael.org>
2018-12-12 20:07:45 +0100

    proftp ssh etc

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,3 +1,10 @@
 node 'client.puppetmichael.org' {
-    include lamp
+    $proftpd_chroot         =  false
+    $proftpd_port           =  21
+    $proftpd_maxinstances   =  25
+    # include lamp
+    # include sshd
+    include motd
+    include proftpd
+
 }
