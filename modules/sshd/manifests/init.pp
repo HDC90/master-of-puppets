c3a29b4ccb21871444ed13800ed89444ae1d3334
root <root@master.puppetmichael.org>
2018-12-12 22:20:06 +0100

    correction bug conf proftpd

---
+++ modules/sshd/manifests/init.pp
@@ -0,0 +1,41 @@
+class ssh::install {
+        package { "openssh":
+                name => $operatingsystem ? {
+                        /(Red Hat|CentOS|Fedora)/ => "openssh-server",
+                        Ubuntu => "openssh-server",
+                        default=> "openssh"
+                 },
+                 ensure => latest,
+        }
+}
+
+class ssh::config {
+        file {"/etc/ssh/sshd_config":
+                        ensure => present,
+                        owner  => 'root',
+                        group  => 'root',
+                        mode   => '0600',
+                        source => "puppet:///modules/sshd/sshd_config",
+                        require=> Class["ssh::install"],
+                        notify => Class["ssh::service"],
+        }
+}
+
+class ssh::service {
+        service { "sshd":
+                        name => $operatingsystem ? {
+                           /(Red Hat|CentOS|Fedora)/ => "sshd",
+                           Ubuntu => "ssh",
+                           default=> "sshd"
+                        },
+                        ensure     => running,
+                        hasstatus  => true,
+                        hasrestart => true,
+                        enable     => true,
+                        require    => Class["ssh::config"],
+        }
+}
+
+class sshd {
+        include ssh::install, ssh::config, ssh::service
+}
